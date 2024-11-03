6e38540fb98118f892fc8caf31cdebd4207d1810
Nishant Bhagat <nishant17bhagat@outlook.com>
2024-11-03 15:20:41 +0530

    bug-fix: light mode pictures added and fix paragrah

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
+"use client";
+
 import Image from "next/image";
 import { Jersey_10 } from "next/font/google";
 import Link from "next/link";
 
 import Github from "@/assets/github.svg";
+import GithubLight from "@/assets/github-light.svg";
 import LinkedIn from "@/assets/linkedin.svg";
+import LinkedInLight from "@/assets/linkedin-light.svg";
 import Email from "@/assets/email.svg";
+import EmailLight from "@/assets/email-light.svg";
+import { useEffect, useState } from "react";
 
 const jersey = Jersey_10({
   weight: ["400"],
@@ -12,6 +18,24 @@ const jersey = Jersey_10({
 });
 
 export default function Home() {
+  const [colorScheme, setColorScheme] = useState("light");
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+
+    const handleColorSchemeChange = (event: any) => {
+      setColorScheme(event.matches ? "dark" : "light");
+    };
+
+    handleColorSchemeChange(mediaQuery);
+
+    mediaQuery.addEventListener("change", handleColorSchemeChange);
+
+    return () => {
+      mediaQuery.removeEventListener("change", handleColorSchemeChange);
+    };
+  }, []);
+
   return (
     <div className="w-full min-h-screen">
       <nav className="flex items-center justify-between m-2 mb-8 md:m-8 lg:my-8 lg:m-auto max-w-[1000px]">
@@ -48,17 +72,15 @@ export default function Home() {
         <h2 className="text-4xl font-bold mb-4">About</h2>
         <p className="leading-loose">
           I am currently student pursuing engineering. Being away from computers
-          always interested me to know to more. But this curiosity now helps me
-          to dig deeper inside and learn more. I wrote my first line of in my
-          higher secondary school when I choose <i>Computer Science</i>
-          course, it was in <i>C++</i> but my personal interest was in web
-          development, but <i>C++</i> knowledge helps me to understand
-          programming concepts better. Before learning how to code I used to
-          review websites source code, that how things work and how they are
-          made. I started web development in freshman year of college, when
-          COVID-19 hit and classes were online, that was the time my inner
-          programmer came out, I started to think in code. This journey is still
-          going on and I am learning new things everyday.
+          always interested me to know them more. And this curiosity now helps
+          me to dig deeper while I learn programming. I wrote my first line of
+          code in my higher secondary school when I choose{" "}
+          <i>Computer Science</i> course, language I learnt was <i>C++</i> but
+          how it is related to web development, but it helped a lot to me to
+          understand programming concepts better. Before learning how to code I
+          used to take a look of website's source code, and try understand how
+          they are made and how they work. I started web development in freshman
+          year at college, when COVID-19 hit and classes were online.
         </p>
       </section>
       <section id="projects" className="mb-8">
@@ -70,39 +92,37 @@ export default function Home() {
         <div className="grid md:grid-cols-2 gap-4">
           <div className="overflow-hidden rounded-md border-[var(--foreground)] border-2 flex items-center">
             <Image
-              src={Github}
+              src={colorScheme == "light" ? GithubLight : Github}
               alt="Github"
               className="h-16 w-16 bg-[var(--foreground)] p-4"
             />
             <span className="px-4 flex flex-col">
               <span className="font-semibold">Github</span>
-              <Link href="https://github.com/onsevenine">
-                https://github.com/onsevenine
-              </Link>
+              <Link href="https://github.com/onsevenine">onsevenine</Link>
             </span>
           </div>
           <div className="overflow-hidden rounded-md border-[var(--foreground)] border-2 flex items-center">
             <Image
-              src={LinkedIn}
+              src={colorScheme == "light" ? LinkedInLight : LinkedIn}
               alt="LinkedIn"
               className="h-16 w-16 bg-[var(--foreground)] p-4"
             />
             <span className="px-4 flex flex-col">
-              <span className="font-semibold">Github</span>
-              <Link href="https://linked.com/in/nishant17bhagat">
-                https://linked.com/in/nishant17bhagat
+              <span className="font-semibold">LinkedIn</span>
+              <Link href="https://linkedin.com/in/nishant17bhagat">
+                nishant17bhagat
               </Link>
             </span>
           </div>
           <div className="overflow-hidden rounded-md border-[var(--foreground)] border-2 flex items-center">
             <Image
-              src={Email}
+              src={colorScheme == "light" ? EmailLight : Email}
               alt="Email"
               className="h-16 w-16 bg-[var(--foreground)] p-4"
             />
             <span className="px-4 flex flex-col">
               <span className="font-semibold">Email</span>
-              <Link href="mailto:onsevenine@gmail.com">Nishant Bhagat</Link>
+              <Link href="mailto:onsevenine@gmail.com">onsevenine</Link>
             </span>
           </div>
         </div>
