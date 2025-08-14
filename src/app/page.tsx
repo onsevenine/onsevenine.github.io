5b14aa1eae0f675aab54ecd2f74adc430e282eff
Nishant Bhagat <nishant17bhagat@outlook.com>
2025-08-14 20:27:08 +0530

    new portfolio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,105 @@
+import Link from "next/link";
 import Image from "next/image";
+
+import githubSvg from "@/assets/github.svg";
+import linkedinSvg from "@/assets/linkedin.svg";
+import twitterSvg from "@/assets/twitter.svg";
+import emailSvg from "@/assets/email.svg";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <main className="max-w-[80ch] my-4 sm:my-8 mx-2 sm:mx-auto space-y-8 tracking-normal">
+      <div className="space-y-2">
+        <h1 className="text-5xl font-bold text-slate-50">Nishant Bhagat</h1>
+        <p className="text-lg text-gray-400">
+          Looking for a role to work in a startup
+        </p>
+        <p className="text-xs text-gray-200">
+          <span className="rounded-full px-2 py-1 bg-gray-200/20 drop-shadow">
+            Backend Development
+          </span>
+          <span className="rounded-full px-2 py-1 bg-gray-200/20 drop-shadow ml-2">
+            DevOps
+          </span>
+        </p>
+      </div>
+      <div className="flex gap-2">
+        <Link href="https://github.com/onsevenine">
+          <Image src={githubSvg} alt="GitHub" className="h-8 w-8" />
+        </Link>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <Link href="https://www.linkedin.com/in/nishant17bhagat/">
+          <Image src={linkedinSvg} alt="LinkedIn" className="h-8 w-8" />
+        </Link>
+
+        <Link href="https://twitter.com/onsevenine">
+          <Image src={twitterSvg} alt="Twitter" className="h-8 w-8" />
+        </Link>
+
+        <Link href="mailto:nishant17bhagat@outlook.com">
+          <Image src={emailSvg} alt="Email" className="h-8 w-8" />
+        </Link>
+      </div>
+      <div>
+        <h2 className="text-2xl font-bold text-slate-100">Experience</h2>
+        <ul className="pl-2">
+          <li>
+            <div className="inline-block  text-gray-100">
+              <Link href="https://cloord.com/">
+                <h3 className="font-semibold underline">Cloord</h3>
+              </Link>
+              <p className="text-sm">Founding member of the company</p>
+              <p className="text-sm">2024-Current</p>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h2 className="text-2xl font-bold text-slate-100">Education</h2>
+        <ul className="pl-2">
+          <li>
+            <div className="inline-block  text-gray-100">
+              <h3 className="font-semibold">
+                Indian Institute of Technology, Guwahati
+              </h3>
+              <p className="text-sm">
+                B. Tech., Electronics and Communication Engineering
+              </p>
+              <p className="text-sm">2021-2025</p>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h2 className="text-2xl font-bold text-slate-100">Projects</h2>
+        <p className="text-gray-100 ml-2">Uploading soon...</p>
+      </div>
+
+      <div>
+        <h2 className="text-2xl font-bold text-slate-100">Skills</h2>
+        <ul className="pl-2 space-y-4">
+          <li>
+            <div className="inline-block text-gray-100">
+              <h3 className="font-semibold">Programming Languages</h3>
+              <p className="text-sm">
+                C, Python, PHP, TypeScript, JavaScript, HTML, CSS
+              </p>
+            </div>
+          </li>
+          <li>
+            <div className="inline-block text-gray-100">
+              <h3 className="font-semibold">Web Frameworks</h3>
+              <p className="text-sm">ReactJS, NextJS, ExpressJS, Laravel*</p>
+            </div>
+          </li>
+          <li>
+            <div className="inline-block text-gray-100">
+              <h3 className="font-semibold">Miscellaneous</h3>
+              <p className="text-sm">Git, Linux, Docker*, AWS*, Bash</p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </main>
   );
 }
